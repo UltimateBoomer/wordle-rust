739871f3e4908cf13bd493a054b87081fdbaf238
UltimateBoomer <caohc2017@gmail.com>
2022-12-26 17:29:51 -0500

    Implement CLI interaction

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@ use std::{io::{self, Write, BufRead}, collections::HashMap};
 
 use colored::{Color, Colorize};
 
-use crate::{WordleSession, WordleGame, LetterValidity};
+use crate::{WordleSession, WordleGame, LetterValidity, GuessResult, GameResult};
 
 pub struct WordleSessionCLI<R, W> {
     session: WordleSession,
@@ -29,12 +29,40 @@ impl<R: BufRead, W: Write> WordleSessionCLI<R, W> {
 
     /// Run the Wordle game.
     pub fn run(&mut self) -> Result<(), io::Error> {
-        println!("Welcome to Wordle in Rust!");
-
-        let mut input = String::new();
-
+        writeln!(&mut self.writer, "Welcome to Wordle in Rust!")?;
+        
         loop {
-            self.reader.read_line(&mut input)?;
+            self.print_board()?;
+            
+            loop {
+                writeln!(&mut self.writer, "Enter your word:")?;
+                
+                let mut input = String::new();
+                self.reader.read_line(&mut input)?;
+                let input = input.trim().to_string();
+
+                let r = self.session.guess(&input);
+                writeln!(&mut self.writer, "You entered: {}", &input)?;
+                match r {
+                    Ok(result) => match result {
+                        GameResult::Cont => break,
+                        GameResult::OutOfGuesses => {
+                            writeln!(&mut self.writer, "Game over: out of guesses.")?;
+                            return Ok(());
+                        },
+                        GameResult::Win => {
+                            writeln!(&mut self.writer, "You win!")?;
+                            return Ok(());
+                        },
+                    },
+                    Err(result) => match result {
+                        GuessResult::AlreadyUsed => writeln!(&mut self.writer, "You've already used that word!")?,
+                        GuessResult::NotInDict => writeln!(&mut self.writer, "That word doesn't exist.")?,
+                        GuessResult::Invalid => writeln!(&mut self.writer, "Invalid word.")?,
+                        _ => continue,
+                    }
+                }
+            }
         }
     }
 
